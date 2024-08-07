44d2c86c9b24e7d30aa9fc453b5bdc987ce7e122
Sanchittanwar30 <111452909+Sanchittanwar30@users.noreply.github.com>
2024-08-07 16:23:08 +0530

    upd1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Intro from "./components/Intro/intro";
 import Navbar from "./components/navbar/navbar";
-
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      <Navbar />
+      <Intro />
     </div>
   );
 }
