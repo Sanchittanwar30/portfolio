b8fe32d2c5cd28210aa8d37a4675c59724931fea
Sanchittanwar30 <111452909+Sanchittanwar30@users.noreply.github.com>
2024-09-22 13:35:18 +0530

    uf2

--- src/components/Works/works.jsx
+++ src/components/Works/works.jsx
@@ -8,21 +8,51 @@ import portfolio5 from "../../assets/portfolio-5.png";
 import portfolio6 from "../../assets/portfolio-6.png";
 
 const Works = () => {
+  const projects = [
+    { img: portfolio1, link: "https://github.com/Sanchittanwar30/Chatapp" },
+    { img: portfolio2, link: "https://github.com/Sanchittanwar30/blog-app" },
+    {
+      img: portfolio3,
+      link: "https://github.com/Sanchittanwar30/Internet-Banking-vf",
+    },
+    {
+      img: portfolio4,
+      link: "https://github.com/Sanchittanwar30/Social-Media",
+    },
+    {
+      img: portfolio5,
+      link: "https://github.com/Sanchittanwar30/world-currency-converter",
+    },
+    {
+      img: portfolio6,
+      link: "https://github.com/Sanchittanwar30/football-league",
+    },
+  ];
+
   return (
     <section id="woRks">
-      <h2 className="worksTitle"> My Portfolio</h2>
+      <h2 className="worksTitle">My Portfolio</h2>
       <span className="workDes">
         Explore a curated collection of my projects to see my skills in action.
+        Please click on the images to explore my projects.
       </span>
       <div className="workImgs">
-        <img src={portfolio1} alt="port1" className="workImg" />
-        <img src={portfolio2} alt="port2" className="workImg" />
-        <img src={portfolio3} alt="port3" className="workImg" />
-        <img src={portfolio4} alt="port4" className="workImg" />
-        <img src={portfolio5} alt="port5" className="workImg" />
-        <img src={portfolio6} alt="port6" className="workImg" />
+        {projects.map((project, index) => (
+          <a
+            key={index}
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={project.img}
+              alt={`port${index + 1}`}
+              className="workImg"
+            />
+          </a>
+        ))}
       </div>
-      <button className="workBtn"> See more </button>
+      <button className="workBtn">See more</button>
     </section>
   );
 };
